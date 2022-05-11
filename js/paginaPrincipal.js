0fb8fe2f4795724b9b380d5a9acb1b7b6ee1189e
KuChauWe <KuChauWe>
2022-05-11 00:31:20 -0500

    Header terminadito 🐱‍🐉

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -1,8 +1,11 @@
+//Para que aparezcan y desaparezcan los filtros
 const btnFiltros = document.querySelector('.btn-filtros')
 
 btnFiltros.addEventListener('click', function() {
     document.getElementById('filtros').classList.toggle('active');
 })
+
+//Para que aparezcan y desaparezcan las opciones "Mi Perfil" y "Cerrar Sesión"
 const btnSesiones = document.querySelector('.btnBoleta')
 
 btnSesiones.addEventListener('click', function() {
